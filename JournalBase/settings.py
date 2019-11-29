6e1bbe79257de6a8c25a43afcbd930e5ed724541
Alexander <alex20k.x@gmail.com>
2019-11-29 18:39:08 +0600

    db settings

--- JournalBase/settings.py
+++ JournalBase/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -75,13 +75,16 @@ WSGI_APPLICATION = 'JournalBase.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'journal_db',
+        'USER': 'postgres',
+        'PASSWORD': 'super',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
-
-# Password validation
+        # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
