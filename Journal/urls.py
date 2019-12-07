6c551a6892d97aae74a3056af25296315af6b32f
Alexander <alex20k.x@gmail.com>
2019-12-07 20:58:49 +0600

    Without notifications

--- Journal/urls.py
+++ Journal/urls.py
@@ -5,6 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('journal/', views.journal, name='journal'),
-    path('notification/', views.notification, name='notification'),
     path('logout/', views.logout_view, name='logout'),
 ]
