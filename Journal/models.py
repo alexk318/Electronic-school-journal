96eaf4abcac34e1afcb9cfb85352b4ea2c2281a9
Alexander <alex20k.x@gmail.com>
2019-12-15 18:09:17 +0600

    __str__ for Class

--- Journal/models.py
+++ Journal/models.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Class(models.Model):
     title = models.CharField(max_length=3, default="", editable=False)
+
+    def __str__(self):
+        return '%s' % self.title.capitalize()
