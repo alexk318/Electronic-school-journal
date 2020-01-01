effd4c5322c66426cbffed5ea4e1e92f4a2a3ae1
Alexander <alex20k.x@gmail.com>
2020-01-01 12:01:44 +0600

    Many-To-Many

--- Journal/models.py
+++ Journal/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class SchoolClass(models.Model):
     title = models.CharField(max_length=3, default="", editable=False, unique=True)
+    students = models.ManyToManyField(User)
 
     def __str__(self):
         return '%s' % self.title.capitalize()
