3bd430dcd11aef3434544c3756495d2426860176
Alexander <alex20k.x@gmail.com>
2020-01-01 11:56:00 +0600

    One-to-many is not needed here

--- Journal/models.py
+++ Journal/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 
 class SchoolClass(models.Model):
     title = models.CharField(max_length=3, default="", editable=False, unique=True)
-    student = models.ForeignKey(User, default="", on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s' % self.title.capitalize()
