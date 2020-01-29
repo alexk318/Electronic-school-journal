06037f0f3eee2d653cb45c451652ed8d81717713
Alexander <alex20k.x@gmail.com>
2020-01-29 19:25:42 +0600

    Right type

--- Journal/models.py
+++ Journal/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-from datetime import datetime
+from datetime import date
 
 
 class SchoolClass(models.Model):
@@ -31,10 +31,10 @@ class Schedule(models.Model):
     schoolclass = models.ForeignKey(SchoolClass, on_delete=models.CASCADE, default="")
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, default="")
 
-    date = models.CharField(max_length=20, default='')
+    date = models.DateField(blank=True, null=True)
 
-    start = models.CharField(max_length=20, default='')
-    end = models.CharField(max_length=20, default='')
+    start = models.TimeField(blank=True, null=True)
+    end = models.TimeField(blank=True, null=True)
 
     def __str__(self):
-        return 'Class: {}, Date: {}, Lesson: {}'.format(self.schoolclass, self.date, self.lesson)
+        return 'Class: {}, Lesson: {}'.format(self.schoolclass, self.lesson)
