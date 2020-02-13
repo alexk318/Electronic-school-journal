25616a51e6a43e7587d8bd27b1d1c9c2c913fce8
Alexander <alex20k.x@gmail.com>
2020-02-13 14:22:46 +0600

    Form for password

--- Journal/forms.py
+++ Journal/forms.py
@@ -2,8 +2,10 @@ from django import forms
 
 
 class AuthForms(forms.Form):
-    username = forms.CharField(label='Username:', max_length=100, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(label='Password:', max_length=200, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(label='Username:', max_length=100, required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(label='Password:', max_length=200, required=True,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
 
 
 class ClassAddForms(forms.Form):
