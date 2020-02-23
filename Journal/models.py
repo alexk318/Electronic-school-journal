42e4fd83139e6c8c944cbadf34841c70e13182f1
Alexander <alex20k.x@gmail.com>
2020-02-23 21:03:25 +0600

    Head teacher for class

--- Journal/models.py
+++ Journal/models.py
@@ -6,6 +6,8 @@ class SchoolClass(models.Model):
     title = models.CharField(max_length=3, default="", unique=True)
     students = models.ManyToManyField(User, blank=True)
 
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='teachers', null=True)
+
     def __str__(self):
         return '%s' % self.title.capitalize()
 
