d28b0341fe9a0eab1132fd742b94707f88c3cad9
Alexander <alex20k.x@gmail.com>
2020-03-08 11:36:03 +0600

    ScheduleAddForms fixed, HomeWorkAddForms fixed.

--- Journal/forms.py
+++ Journal/forms.py
@@ -1,9 +1,6 @@
 from django import forms
-from .models import Day, Lesson, SchoolClass, Schedule, UserImage
+from .models import Day, Lesson, SchoolClass, Schedule, UserImage, Teacher, HomeWork
 from django.contrib.auth.models import User, Group
-
-days = Day.objects.all()
-days_choices = [tuple([d, d]) for d in days]
 
 lessons = Lesson.objects.all()
 lessons_choices = [tuple([l, l]) for l in lessons]
@@ -13,14 +10,16 @@ groups_choices = [tuple([g, g]) for g in groups]
 
 users = User.objects.all()
 
-teacher = Group.objects.get(name='Teacher')
+teachers = Teacher.objects.all()
 
-teachers = [user for user in teacher.user_set.all() if user.teachers.first() is None]
-teachers_choices = [tuple([t.id, t.first_name + ' ' + t.last_name]) for t in teachers]
-teachers_choices.insert(0, (None, '-------'))
+# teachers = [user for user in teacher.user_set.all() if user.teachers.first() is None]
+# teachers_choices = [tuple([t.id, t.first_name + ' ' + t.last_name]) for t in teachers]
+# teachers_choices.insert(0, (None, '-------'))
 
-schedules = Schedule.objects.all()
-schedules_choices = [tuple([s.id, s]) for s in schedules if s.homework_set.first() is None]
+teachers_choices = []
+
+# schedules = Schedule.objects.filter(homework=None).filter(lesson_teacher).all()
+# schedules_choices = [tuple([s, s]) for s in schedules]
 
 schoolclasses = SchoolClass.objects.all()
 schoolclass_choices = [tuple([s, s]) for s in schoolclasses]
@@ -47,6 +46,17 @@ class UserAddForms(forms.ModelForm):
             'last_name': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
             'email': forms.TextInput(attrs={'class': 'form-control', 'required': True}),
         }
+
+
+# class TeacherForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Teacher
+#         fields = ['schoolclasses', 'lessons']
+#         widgets = {
+#             'schoolclasses': forms.CheckboxSelectMultiple(),
+#             'lessons': forms.CheckboxSelectMultiple()
+#         }
 
 
 class UserEditForms(forms.Form):
@@ -80,17 +90,18 @@ class ClassAddForms(forms.ModelForm):
         }
 
 
-class ScheduleAddForms(forms.Form):
-    day_week = forms.CharField(label='Day of the week:', widget=forms.Select(choices=days_choices,
-                                                                             attrs={'class': 'form-control'}))
-    lesson = forms.CharField(label='Lesson:', widget=forms.Select(choices=lessons_choices, attrs={'format': '%Y-%m-%d',
-                                                                                                  'class': 'form-control'})
-                             )
-
-    lesson_date = forms.DateField(label='Lesson date:', widget=forms.SelectDateWidget(attrs={'class': 'form-control'}))
-
-    lesson_start = forms.TimeField(widget=forms.TimeInput(format='%H:%M', attrs={'class': 'form-control'}))
-    lesson_end = forms.TimeField(widget=forms.TimeInput(format='%H:%M', attrs={'class': 'form-control'}))
+class ScheduleAddForms(forms.ModelForm):
+    class Meta:
+        model = Schedule
+        fields = ['day', 'lesson', 'lessonteacher', 'date', 'start', 'end']
+        widgets = {
+            'day': forms.Select(choices=Day.objects.all(), attrs={'class': 'form-control'}),
+            'lesson': forms.Select(choices=Lesson.objects.all(), attrs={'class': 'form-control'}),
+            'lessonteacher': forms.Select(choices=teachers, attrs={'class': 'form-control'}),
+            'date': forms.DateInput(attrs={'class': 'form-control'}),
+            'start': forms.TimeInput(attrs={'class': 'form-control'}),
+            'end': forms.TimeInput(attrs={'class': 'form-control'})
+        }
 
 
 class LessonAddForms(forms.ModelForm):
@@ -102,9 +113,10 @@ class LessonAddForms(forms.ModelForm):
         }
 
 
-class HomeWorkForms(forms.Form):
-    schoolclass = forms.CharField(label='Schoolclass:', widget=forms.Select(choices=schoolclass_choices,
-                                                                            attrs={'class': 'form-control'}))
-    schedule = forms.CharField(label='Schedule:', widget=forms.Select(choices=schedules_choices,
-                                                                      attrs={'class': 'form-control'}))
-    text = forms.CharField(label='Text:', widget=forms.Textarea(attrs={'class': 'form-control'}))
+class HomeWorkForms(forms.ModelForm):
+    class Meta:
+        model = HomeWork
+        fields = ['schedule', 'text']
+        widgets = {
+            'schedule': forms.Select(attrs={'class': 'form-control'}),
+        }
