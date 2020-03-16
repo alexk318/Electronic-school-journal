a802100848e1aef7c5c678e70a8d93f40e2a3772
Alexander <alex20k.x@gmail.com>
2020-03-16 23:23:55 +0600

    li18n patterns

--- JournalBase/urls.py
+++ JournalBase/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.conf.urls.i18n import  i18n_patterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('Journal.urls'))
+path('admin/', admin.site.urls),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('Journal.urls'))
+)
 
