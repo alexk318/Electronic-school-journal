f7cbab0f23942f1d46e7e9b5a1fd1fafcd15240c
Alex <alex20k.x@gmail.com>
2020-05-01 11:35:18 +0600

    New urls

--- Journal/urls.py
+++ Journal/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('journal/profile/<int:user_id>', views.profile, name='profile'),
     path('journal/profile/<int:user_id>/delete', views.user_delete, name='user_delete'),
 
-    path('journal/', views.journal, name='journal'),
     path('journal/schedule/', views.schedule, name='schedule'),
 
     path('journal/schedule/<str:class_title>/<str:month_title>/<str:week_numbers>', views.class_schedule,
@@ -27,6 +26,27 @@ urlpatterns = [
     path('journal/lessons/', views.lessons, name='lessons'),
 
     path('journal/homework', views.homework, name='homework'),
+    path(
+        'journal/homework/assign/<int:ih_id>/<int:submithomework_id>/<int:homework_id>/<int:schoolclass_id>/'
+        '<int:student_id>',
+        views.assign_grade, name='assign_grade'),
+
+    path('journal/homework/submit/<int:homework_id>', views.submit_homework, name='submit_homework'),
+
+    path('journal/homework/check/<int:homework_id>', views.check_homework, name='check_homework'),
+
+    path('journal/download/<str:filepath>/<str:filename>', views.download, name='download'),
+
+    path('journal/homework/individual/<int:schoolclass_id>/<int:student_id>', views.individual_homework,
+         name='individual_homework'),
+    path('journal/homework/individual/submit/<int:individualhomework_id>', views.submit_individualhomework,
+         name='submit_individualhomework'),
+
+    path('journal/grades/', views.grades, name='grades'),
+
+    path('journal/homework/close/<int:h_id>', views.close_homework, name='close_homework'),
+    path('journal/homework/individual/close/<int:h_id>', views.close_individualhomework,
+         name='close_individualhomework'),
 
     path('logout/', views.logout_view, name='logout'),
 ]
@@ -34,4 +54,3 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-
