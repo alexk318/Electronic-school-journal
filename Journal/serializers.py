0973298dcbf94ae0d7e07d7de16b91e80d525a57
Alex <alex20k.x@gmail.com>
2020-05-11 21:21:11 +0600

    new students serializers

--- Journal/serializers.py
+++ Journal/serializers.py
@@ -5,29 +5,13 @@ from django.contrib.auth.models import User, Group
 # объекты моделей, в типы данных Python, которые затем можно легко преобразовать в JSON, XML или другие content types.
 
 
-student = Group.objects.get(name='Student')
-
-
 class ScheduleSerializer(serializers.Serializer):
     date = serializers.DateField()
     start = serializers.TimeField()
     end = serializers.TimeField()
 
 
-class StudentsSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
-
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-
-    email = serializers.EmailField()
-
-    def create(self, validated_data):
-        user = User.objects.create_user(username=validated_data['username'], password=validated_data['password'],
-                                        first_name=validated_data['first_name'], last_name=validated_data['last_name'],
-                                        email=validated_data['email'])
-
-        student.user_set.add(user)
-
-        return user
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
