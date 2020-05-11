bc69b25d1bd02fe67e9ed0abbcb32c930dd8c3cb
Alex <alex20k.x@gmail.com>
2020-05-11 21:20:30 +0600

    urls template for api

--- JournalBase/urls.py
+++ JournalBase/urls.py
@@ -19,5 +19,8 @@ from django.urls import include, path
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
     path('admin/', admin.site.urls),
-    path('', include('Journal.urls'))
+
+    path('', include('Journal.urls')),
+
+    path('api/v1/students/', include('Journal.urlsrest')),
 ]
